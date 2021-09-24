05549c2ad5f6ed5b8efe5f94389786f49feb7d43
Abul Kashem <kasemshishir1100@gmail.com>
2021-09-24 22:10:39 +0600

    v1.0

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn");
+const jokeEl = document.querySelector("#joke");
+
+let joke = [];
+
+async function getJoke() {
+  try {
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any");
+    joke = await response.json();
+    changeHtml();
+  } catch (err) {}
+}
+getJoke();
+
+const changeHtml = function () {
+  if (joke.delivery === undefined || joke.delivery === undefined) {
+    getJoke();
+  } else {
+    jokeEl.textContent = joke.setup + " " + joke.delivery;
+  }
+};
+btn.addEventListener("click", getJoke);
